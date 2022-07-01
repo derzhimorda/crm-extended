35d07dfbfb4793c5c5ddd9227c31c57ddfbd1b5b
jackinsoon <jackinson1086@gmail.com>
2022-07-01 18:15:00 +0200

    User Deals page init

---
+++ frontend/src/app/pages/users/user/user-deals/user-deals.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-deals',
+  templateUrl: './user-deals.component.html',
+  styleUrls: ['./user-deals.component.scss']
+})
+export class UserDealsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
